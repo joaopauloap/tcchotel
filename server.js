81b199a01a5ba1c4cb842f292e626824d94673b8
joapauloap <jpmail98@gmail.com>
2020-11-28 18:34:54 -0300

    build

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express()
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static('web'))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 
